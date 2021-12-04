cd620d8f0183867f4123d68244bbb7ff58f5cc1a
Ashley Kolodziej <kolodz10@bu.edu>
2021-12-04 16:57:50 -0500

    Saner linting rules for JS

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
 	"extends": "airbnb-base",
 	"rules": {
 		"linebreak-style": 0,
+		"no-tabs": 0,
+		"indent": [2, "tab"],
+		"guard-for-in": 0,
+		"no-restricted-syntax": 0,
+		"no-plusplus": 0,
 	},
 	"env": {
 		"browser": true,
