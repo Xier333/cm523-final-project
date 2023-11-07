dbe1be9aa8481beafd000c64d8463963bf00cf2f
Ashley Kolodziej <kolodz10@bu.edu>
2023-11-06 22:44:11 -0500

    Update with tests

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,7 @@
-// This file is eventually where you'll learn to write JavaScript.
-// For right now, you don't need to worry about it.
+// This is where you should write all JavaScript
+// for your project. Remember a few things as you start!
+// - Do not use var - use let or const instead
+// - Do not use jQuery - use JavaScript instead
+// - Do not use onclick - use addEventListener instead
+// - Run npm run test regularly to check autograding
+// - You'll need to link this file to your HTML :)
