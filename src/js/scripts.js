24b4c8d9652acc6c0b892f3f5daf08a0ff0bd17d
Ashley Kolodziej <kolodz10@bu.edu>
2023-11-06 22:54:05 -0500

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 // This is where you should write all JavaScript
 // for your project. Remember a few things as you start!
-// - Do not use var - use let or const instead
+// - Use let or const for all variables
 // - Do not use jQuery - use JavaScript instead
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
